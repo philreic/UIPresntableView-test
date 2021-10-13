692493668072b6d56e41016f1eb833411b5b6bad
philreic <91380439+philreic@users.noreply.github.com>
2021-10-13 13:07:30 +0200

    Initial Commit

---
+++ UIPresntableView test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIPresntableView test
+//
+//  Created by Philippe Reichen on 10/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIPresntableView test/UIPresntableView_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPresntableView_testApp.swift
+//  UIPresntableView test
+//
+//  Created by Philippe Reichen on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct UIPresntableView_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
